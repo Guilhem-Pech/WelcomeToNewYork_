03e50a8b611d676544e3c440afd46b15d7602506
edouard-lebeau <edouard.lebeau@etu.univ-amu.fr>
2019-04-12 10:57:17 -0400

    wip

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Mirror;
 
-public class PauseMenu : MonoBehaviour
+public class PauseMenu : NetworkBehaviour
 {
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    private NetworkManager manager;
 
 
     void Update()
@@ -46,6 +48,14 @@ public class PauseMenu : MonoBehaviour
     public void MenuGame()
     {
         Time.timeScale = 1;
+
+        manager = GameObject.FindObjectOfType<NetworkManager>();
+        if (isServer)
+        {
+            Debug.Log(manager);
+            manager.StopHost();
+            Debug.Log("SERVEUR STOP");
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
