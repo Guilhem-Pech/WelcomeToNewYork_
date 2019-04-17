00c3c135c30d7482e069cf01de55b3eafac09fb9
Ajirogg <32861990+Ajirogg@users.noreply.github.com>
2019-04-17 14:17:01 -0400

    Revert "first commit UI FULL KS PUTAIN"
    This reverts commit 7f7345a66c872888e7e2966c5ed681b6ecff05f0.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -8,8 +8,6 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
-    public GameObject optionsMenu;
-    public GameObject mainMenu;
 
 
     void Update()
@@ -30,14 +28,12 @@ public class PauseMenu : MonoBehaviour
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
-        optionsMenu.SetActive(false);
         GameIsPaused = false;
     }
 
     public void Pause()
     {
         pauseMenuUI.SetActive(true);
-        mainMenu.SetActive(true);
         GameIsPaused = true;
     }
 
